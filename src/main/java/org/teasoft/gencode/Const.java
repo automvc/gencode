93a7f72cfbe31cbb6649dccb7d98590053e9e727
Kingstar <honeysoft@126.com>
2023-09-13 00:09:48 +0800

    update for V2.1.8

--- src/main/java/org/teasoft/gencode/Const.java
+++ src/main/java/org/teasoft/gencode/Const.java
@@ -7,17 +7,17 @@ package org.teasoft.gencode;
  */
 public class Const {
 
-	public static String MSG_OK = "SUCCESS";
-	public static String MSG_ERROR = "ERROR";
-	public static String CODE_OK = "1200";
-	public static String CODE_ERROR = "1500";
+	public static final String MSG_OK = "SUCCESS";
+	public static final String MSG_ERROR = "ERROR";
+	public static final String CODE_OK = "1200";
+	public static final String CODE_ERROR = "1500";
 
-	public static String CODE_FAILED = "1501";
-	public static String MSG_INSERT_ERROR = "insert failed";
-	public static String MSG_UPDATE_ERROR = "update failed";
-	public static String MSG_DELETE_ERROR = "delete failed";
+	public static final String CODE_FAILED = "1501";
+	public static final String MSG_INSERT_ERROR = "insert failed";
+	public static final String MSG_UPDATE_ERROR = "update failed";
+	public static final String MSG_DELETE_ERROR = "delete failed";
 
-	public static String CODE_NONE = "1404";
-	public static String MSG_NONE = "Affected rows: 0";
+	public static final String CODE_NONE = "1404";
+	public static final String MSG_NONE = "Affected rows: 0";
 
 }
